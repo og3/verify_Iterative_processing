575b774aaba79b7153d811b8c927eeb8ae7a8958
noriyuki togashi <>
2020-02-16 09:11:16 +0900

    userモデルを用意し、10000のuserを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10000.times do |i|
+  User.create!(
+     email: "#{i}番目@gmail.com",
+     name: "#{i}人目のテスト太郎",
+  )
+end
